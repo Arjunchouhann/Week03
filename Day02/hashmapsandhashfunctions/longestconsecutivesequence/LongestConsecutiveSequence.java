2c5c9af1aa2c37d348e6766958721e03cff22a60
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-03 11:05:13 +0530

    [Arjun] Add.longest consecutive sequence

---
+++ Day02/hashmapsandhashfunctions/longestconsecutivesequence/LongestConsecutiveSequence.java
@@ -0,0 +1,39 @@
+package Day02.hashmapsandhashfunctions.longestconsecutivesequence;
+
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+    public static int longestConsecutive(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : numSet) {
+            // Check if it's the start of a sequence
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                // Count consecutive numbers
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                // Update the longest streak
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        int result = longestConsecutive(nums);
+        System.out.println("The length of the longest consecutive elements sequence is: " + result);
+    }
+}
