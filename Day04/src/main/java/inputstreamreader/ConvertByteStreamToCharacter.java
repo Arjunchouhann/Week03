72a9ee99a8f9614378d4a999e76f4505a92508a4
Arjunchouhann <chouhanarjun05112003@gmail.com>
2025-02-08 11:20:43 +0530

    [Arjun] Add.convert byte stream to character

---
+++ Day04/src/main/java/inputstreamreader/ConvertByteStreamToCharacter.java
@@ -0,0 +1,28 @@
+package inputstreamreader;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class ConvertByteStreamToCharacter {
+    public static void main(String[] args) {
+        String file = "src/sample";
+
+        //try block to throw the exception object the program might throw
+        try(FileInputStream fileInputStream = new FileInputStream(file);
+            //wrapping FileInputStream in an InputStreamReader
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+            BufferedReader bufferReader = new BufferedReader(inputStreamReader)) {
+            String line ;
+            while ((line = bufferReader.readLine()) != null){
+                System.out.println(line);
+            }
+            //catch block handles all the exceptions
+        }catch (FileNotFoundException e){
+            System.out.println("File not fount exception" + e.getMessage());
+        } catch (UnsupportedEncodingException e){
+            System.out.println("UnsupportedEncodingException found" + e.getMessage());
+        } catch (IOException e){
+            System.out.println("IOException found" + e.getMessage());
+        }
+    }
+}
